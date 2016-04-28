ea1b914e73e9a7bbf141cc8fc673af5b039803b5
Alexey Raga <alexey.raga@gmail.com>
2016-04-29 06:49:56 +1000

    build scripts with nix

---
+++ circle.build/stack-build.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+
+stack setup
+stack clean
+stack build
---
+++ circle.build/stack-test.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+. $HOME/.nix-profile/etc/profile.d/nix.sh
+
+stack test
