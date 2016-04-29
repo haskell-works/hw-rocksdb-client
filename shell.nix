651d690e9f32109cbad7821ecd219571cd952c49
Alexey Raga <alexey.raga@gmail.com>
2016-04-29 23:35:33 +1000

    setting up args depending on env

--- shell.nix
+++ shell.nix
@@ -1,21 +1,38 @@
-with (import <nixpkgs> {}); with builtins;
+with (import <nixpkgs> {});
+with builtins;
 
-stdenv.mkDerivation {
+let
+  # native packages installed by nix in CCI, but not locally
+  nativePackages = [ rocksdb ];
+  
+  # Packages that are downloaded from Hackage in CCI but referenced locally otherwise
+  # in a format of { package1 = localPath2; package2 = localPath2; }
+  localPackages  = {};
+  
+  # Other non-stackage dependencies, such as LTE itself or/and hackage packages
+  otherPackages  = [ haskell.packages.lts-5_9.ghc ];
+  
+  ################### INTERNAL HANDWAVING #####################
+  isCI = if builtins.getEnv "CI" != "" then true else false;
+  
+  localDeps = if isCI
+      then builtins.attrNames localPackages
+      else builtins.attrValues localPackages;
+  
+  packages = if isCI 
+      then builtins.concatLists [ nativePackages localDeps ]
+      else builtins.concatLists [ localDeps ];
+      
+  stackArgs = if isCI
+      then builtins.foldl' (x: y: x + " --extra-lib-dirs=${y}/lib --extra-include-dirs=${y}/include") "" nativePackages
+      else " --extra-lib-dirs=/usr/local/lib --extra-include-dirs=/usr/local/include";
+  
+  ######################## DERIVIATION ########################
+in stdenv.mkDerivation {
+  
+  name = "rocksdbClientEnv";
 
-  name = "myEnv";
+  buildInputs = builtins.concatLists [ packages otherPackages ];
 
-  buildInputs = if builtins.getEnv "CI" == "true" 
-    then [
-      rocksdb
-      haskell.packages.lts-5_9.ghc
-    ]
-    else [
-      haskell.packages.lts-5_9.ghc
-    ];
-   
-
-  STACK_IN_NIX_EXTRA_ARGS
-      = " --extra-lib-dirs=${rocksdb}/lib" 
-      + " --extra-include-dirs=${rocksdb}/include" 
-  ;
+  STACK_IN_NIX_EXTRA_ARGS = stackArgs;
 }
