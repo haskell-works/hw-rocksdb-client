ca72d511dcd6ccccea0d9608390f1abbf64ab032
Alexey Raga <alexey.raga@gmail.com>
2017-04-02 17:12:24 +1000

    maybe faster CI

--- circle.build/build-rocksdb.sh
+++ circle.build/build-rocksdb.sh
@@ -2,15 +2,19 @@
 
 cd ~
 
+_version=5.2.1
+
 if [ ! -d "rocksdb" ]; then
   git clone https://github.com/facebook/rocksdb rocksdb
 fi
 
 cd rocksdb
-git pull
-git reset --hard v5.2.1
 
-make static_lib
-sudo make install
-make shared_lib
+if ! ls librocksdb.${_version}.* 1> /dev/null 2>&1; then
+  git pull
+  git reset --hard v${_version}
+  make static_lib
+  make shared_lib
+fi
+
 sudo make install
